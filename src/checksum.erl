3fa5464620c4645119f644c7d8ae0f0902c4f77f
Rick Payne <rickp@rossfell.co.uk>
2016-08-16 14:29:59 +0100

    EQC Changes from Thomas
    Add checksum eqc test
    rebar3 eqc plugin

--- src/checksum.erl
+++ src/checksum.erl
@@ -24,17 +24,25 @@
 
 -module(checksum).
 
--export([checksum/1, checksum_1/1, start/0, init/0]).
+-export([checksum/1, checksum_1/1, start/0, init/1]).
 
 -define(INT16MAX, 65535).
 
 start() ->
-    spawn(checksum, init, []).
+    Ref = make_ref(),
+    Self = self(),
+    Pid = spawn(checksum, init, [{Self, Ref}]),
+    receive
+      {Ref, started} -> Pid
+      after 5000 ->
+          erlang:error("cannot start checksum driver")
+    end.
 
-init() ->
-    erl_ddll:load_driver(code:priv_dir(etcpip), "checksum"),
+init({Parent,Ref}) ->
+    ok = erl_ddll:load_driver(code:priv_dir(etcpip), "checksum"),
     Port = open_port({spawn, checksum}, [binary]),
     register(checksum, self()),
+    Parent ! {Ref, started},
     loop(Port).
 
 checksum(Packet) ->
